40b84223c3fa45208509ae54430b24dbc93d3a2b
SamuelArdi <fiicery.work@gmail.com>
2026-09-12 16:30:05 +0700

    checked if the user has already run the program

--- build.bat
+++ build.bat
@@ -6,9 +6,12 @@ IF NOT EXIST build (
 )
 
 IF %1==rel (
+  ECHO Building For Release
   cmake -B build -DCMAKE_BUILD_TYPE=Release
   cmake --build build --config Release
+  build\release\my_application.exe
 ) ELSE (
+  ECHO Building for Debug
   cmake -B build
   cmake --build build
   build\debug\my_application.exe
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "app-window.h"
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
 #ifdef __ANDROID__
 extern "C" void slint_main()
 #else
@@ -7,6 +11,15 @@ int main(int argc, char **argv)
 #endif
 
 {
+  std::filesystem::path fileName = ".firstrun";
+  bool firstRun = !std::filesystem::exists(fileName); // reverses so if its NOT found, then false instead of true
+  if (firstRun) {
+    std::ofstream file(fileName);
+    file.close();
+  }
+
   auto ui = AppWindow::create();
   ui->run();
+
+  return 0;
 }
