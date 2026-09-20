f8e43a9768c260b145fe898ab73daf1b7007b5ce
SamuelArdi <fiicery.work@gmail.com>
2026-09-20 18:29:14 +0700

    changed output file to the root folder

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,20 @@ int main() {
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(screenWidth, screenHeight, "TimeOn");
-
   SetTargetFPS(60);
 
   while(!WindowShouldClose()) {
-    // set the minimum window size
+    // set the minimum and maximum window size
     SetWindowMinSize(520, 640);
+    SetWindowMaxSize(1080, 1440);
 
     // update changes to the window size
-    screenWidth = GetScreenWidth();
-    screenHeight = GetScreenHeight();
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
 
     BeginDrawing();
       ClearBackground(RAYWHITE);
-      DrawText("Hello World!", screenWidth/2, screenHeight/2, 20, LIGHTGRAY);
+      DrawText("Hello World", screenWidth/2, screenHeight/2, 20, MAROON);
     EndDrawing();
   }
 
