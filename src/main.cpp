5a5dc6a6d6572b4c10c7a6950c43a2a7034ac6f8
SamuelArdi <fiicery.work@gmail.com>
2026-09-20 19:00:59 +0700

    add assets

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@ int main() {
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(screenWidth, screenHeight, "TimeOn");
+
+  Texture2D bg = LoadTexture("assets/images/bg.jpg");
+  Texture2D vhs = LoadTexture("assets/images/vhs_filter.jpg");
+
+  Texture2D clock = LoadTexture("assets/images/rect_clock.png");
+  Texture2D hourHand = LoadTexture("assets/images/hour_hand.svg");
+  Texture2D minuteHand = LoadTexture("assets/images/minute_hand.svg");
+  
+  Texture2D timecard = LoadTexture("assets/images/timecard.jpg");
+
   SetTargetFPS(60);
 
   while(!WindowShouldClose()) {
@@ -18,10 +28,16 @@ int main() {
     int screenHeight = GetScreenHeight();
 
     BeginDrawing();
-      ClearBackground(RAYWHITE);
-      DrawText("Hello World", screenWidth/2, screenHeight/2, 20, MAROON);
+      DrawTexture(bg, 0, 0, BLANK);
     EndDrawing();
   }
+
+  UnloadTexture(bg);
+  UnloadTexture(vhs);
+  UnloadTexture(clock);
+  UnloadTexture(hourHand);
+  UnloadTexture(minuteHand);
+  UnloadTexture(timecard);
 
   CloseWindow();
   return 0;
