3f0e1e9d023a9bfe9c0c9222807c0bb155317c84
SamuelArdi <fiicery.work@gmail.com>
2026-09-20 17:30:52 +0700

    changing from slint to raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,6 @@
-#include "app-window.h"
+#include <raylib.h>
 
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-
-#ifdef __ANDROID__
-extern "C" void slint_main()
-#else
-int main(int argc, char **argv)
-#endif
-
-{
-  std::filesystem::path fileName = ".firstrun";
-  if (bool firstRun = !std::filesystem::exists(fileName)) {
-    std::ofstream file(fileName);
-    file.close();
-  }
-
-
-
-  auto ui = AppWindow::create();
-  ui->run();
+int main() {
 
   return 0;
 }
